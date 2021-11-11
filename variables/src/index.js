a32e86dadefce4d5a911b1c84a60a767cc420e2e
alejandro <alejandrogalaz21@gmail.com>
2021-11-11 11:15:47 -0600

    ignore files

--- variables/src/index.js
+++ variables/src/index.js
@@ -1,1 +1,14 @@
-console.log("hello world!");
+const var1 = null
+const var2 = undefined
+let var3 = [
+  {
+    name: 'alex',
+    age: 32,
+  },
+  {
+    name: 'leo',
+    age: 33,
+  },
+]
+
+console.table(var3)
