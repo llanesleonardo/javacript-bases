bd06be82f2ccda083e8da941951459bfdb64f664
alejandro <alejandrogalaz21@gmail.com>
2021-11-25 20:18:41 -0600

    ad updates

---
+++ arrayCardio2/src/index.test.js
@@ -0,0 +1,9 @@
+import { sum } from './helpers'
+
+
+describe('Tests Cardio Day 1', () => {
+  test('My 1st Set Of Test', () => {
+  test('Sum up the instances of each of these', () => {
+    expect(sum(1, 1)).toEqual(2)
+  })
+})
